2ee9362e582e39f9b931bca8d91bab0f31fb700d
MonserrateConnor69 <mrgobasco@tip.edu.ph>
2024-11-30 04:14:48 +0800

    Delete Barbershop directory

--- Barbershop/src/application/RegularAppointment.java
+++
@@ -1,9 +0,0 @@
-package application;
-
-public class RegularAppointment extends Appointment {
-
-    
-    public RegularAppointment(String customerName, String contactInfo, String barber, String date, String time, String barbershop) {
-        super(customerName, contactInfo, barber, date, time, barbershop, 0.0);  
-    }
-}
--- Barbershop/src/module-info.java
+++
@@ -1,5 +0,0 @@
-module Barbershop {
-	requires javafx.controls;
-	
-	opens application to javafx.graphics, javafx.fxml;
-}
